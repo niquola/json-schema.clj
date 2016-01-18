716b911086794c125fb1cfe62da905b63d8749ef
niquola <niquola@gmail.com>
2016-01-18 20:17:21 +0000

    init

--- src/json_schema/core.clj
+++ src/json_schema/core.clj
@@ -1,6 +1,72 @@
 (ns json-schema.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn add-error [ctx err]
+  (update-in ctx [:errors] conj err))
+
+(defn minMaxItems [key rule schema subj ctx]
+  (if-not (vector? subj)
+    ctx
+    (let [op (cond (= key :minItems) >=
+                   (= key :maxItems) <=)
+          cnt (count subj)]
+      (if (op cnt rule)
+        ctx
+        (add-error ctx {:desc     :maxItems
+                        :actual   cnt
+                        :expected (str key " then " rule)})))))
+
+
+(defn minMaxLength [key rule schema subj ctx]
+  (println "length" key rule schema subj ctx)
+  (if-not (string? subj)
+    ctx
+    (let [op   (cond (= key :minLength) <=
+                     (= key :maxLength) >=)
+          cnt  (.count (.codePoints subj))]
+      (if (op rule cnt)
+        ctx
+        (add-error ctx {:desc     "maxLength"
+                        :actual   cnt
+                        :expected (str key " then " rule)})))))
+
+(defn minimum [key rule schema subj ctx]
+  (if-not (number? subj)
+    ctx
+    (let [op (if (:exclusiveMinimum schema) > >=)]
+      (if (op subj rule)
+        ctx
+        (add-error ctx {:desc "maxLength"
+                        :actual subj
+                        :expected (str "more then " rule)})))))
+
+
+(defn skip [key rule schema subj ctx] ctx)
+
+(def validators
+  {:maxItems minMaxItems
+   :minItems minMaxItems
+
+   :maxLength minMaxLength
+   :minLength minMaxLength
+
+   :minimum minimum
+   :exclusiveMinimum skip
+   :exclusiveMaximum skip})
+
+(defn validate* [schema subj ctx]
+  (if (map? schema)
+    (reduce
+     (fn [ctx [key rule]]
+       (if-let [h (get validators key)]
+         (h key rule schema subj ctx)
+         (add-error ctx {:desc "Unknown key " :details key})))
+     ctx
+     schema)))
+
+(defn validate [schema subj]
+  (let [res (validate* schema subj {:errors []})]
+    (if (empty? (:errors res))
+      true
+      false
+      #_(:errors res))))
+
--- test/json_schema/core_test.clj
+++ test/json_schema/core_test.clj
@@ -1,7 +1,24 @@
 (ns json-schema.core-test
-  (:require [clojure.test :refer :all]
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [json-schema.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(defn filter-by-name [nm] (fn [fl] (re-matches nm (.getPath fl))))
+
+(deftest a-schema-test
+  (doseq [test-file  (->> "draft4"
+                          io/resource
+                          io/file
+                          file-seq
+                          (filter (filter-by-name #"^.*(max|min).*")))]
+    (when (.isFile test-file)
+      (let [test-case (json/parse-string (slurp (.getPath test-file)) keyword)]
+        (doseq [scenario test-case]
+          (testing (:description scenario)
+            (doseq [test-item (:tests scenario)]
+              (println (:valid test-item))
+              (is (= (:valid test-item)
+                     (validate (:schema scenario) (:data test-item)))
+                  (pr-str (assoc test-item :schema (:schema scenario)))))))))))
