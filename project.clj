afab620eae9a3af4b6a02e46f10488725dd60993
niquola <niquola@gmail.com>
2017-11-13 17:47:02 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject json-schema "0.2.0-RC3"
+(defproject json-schema "0.2.0-RC4"
   :description "Conformant json-schema implementation"
   :url "https://github.com/niquola/json-schema.clj "
   :license {:name "Eclipse Public License"
