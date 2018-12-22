e05ba13186eb39f64e6d94c41a841511c615ddc0
niquola <niquola@gmail.com>
2018-12-22 18:00:27 +0300

    macro progress

--- src/json_schema/macroschema.clj
+++ src/json_schema/macroschema.clj
@@ -1,12 +1,8 @@
 (ns json-schema.macroschema
   (:require [clojure.string :as str]))
 
-
-
-`(cond-> ~'ctx
-   (nil? (get ~'subj :a) (add-error ~'ctx path ))
-   (nil? (get ~'subj :a))
-   (nil? (get ~'subj :a)))
+(defn add-error [ctx msg]
+  (update ctx :errors conj {:desc msg :path (:path ctx)}))
 
 (declare expand)
 
@@ -15,26 +11,36 @@
 (defmethod expand-key :type
   [_ _ tp _]
   (cond
-    (= "object" (name tp))
-    `(cond->
-         (not (map? ~'subj)) (add-error (str "Expected an object, got " ~'subj)))
+    (string? tp)
+    (cond
+      (= "object" (name tp))
+      `(cond-> (not (map? ~'subj)) (add-error (str "Expected an object, got " ~'subj)))
 
-    (= "string" (name tp))
-    `(cond->
-         (not (string? ~'subj)) (add-error (str "Expected a string, got " ~'subj)))
+      (= "string" (name tp))
+      `(cond-> (not (string? ~'subj)) (add-error (str "Expected a string, got " ~'subj)))
 
-    (= "array" (name tp))
-    `(cond->
-         (not (sequential? ~'subj)) (add-error (str "Expected an array, got " ~'subj)))))
+      (= "integer" (name tp))
+      `(cond-> (not (int? ~'subj)) (add-error (str "Expected a integer, got " ~'subj)))
+
+      (= "number" (name tp))
+      `(cond-> (not (number? ~'subj)) (add-error (str "Expected a integer, got " ~'subj)))
+
+      (= "array" (name tp))
+      `(cond-> (not (sequential? ~'subj)) (add-error (str "Expected an array, got " ~'subj)))
+      :else
+      (assert false (pr-str  "TODO type" tp)))
+    :else
+    (assert false (pr-str  "TODO type" tp))))
 
 (defmethod expand-key :required
   [_ _ fields _]
-  (let [preds (mapcat
-               (fn [f]
-                 (let [msg (str  f " is required")]
-                   [`(nil? (get ~'subj ~(keyword f))) `(add-error  ~msg)])
-                 ) fields)]
-    `(cond-> (map? ~'subj) (cond-> ~@preds))))
+  (cond (sequential? fields)
+        (let [preds (mapcat
+                     (fn [f]
+                       (let [msg (str  f " is required")]
+                         [`(nil? (get ~'subj ~(keyword f))) `(add-error  ~msg)])
+                       ) fields)]
+          `(cond-> (map? ~'subj) (cond-> ~@preds)))))
 
 (defn push-path [ctx & ks]
   (update ctx :path into ks))
@@ -46,7 +52,7 @@
   [cctx _ props _]
   (let [props-e (mapcat
                  (fn [[k v]]
-                   (let [lmbd (expand (push-path cctx :properties k) v)]
+                   (let [lmbd (expand (push-path cctx (keyword k)) v)]
                      [`(not (nil? (get ~'subj ~(keyword k))))
                       `(-> (push-path ~(keyword k))
                            (~lmbd (get ~'subj ~(keyword k)))
@@ -64,31 +70,68 @@
   [cctx _ rf _]
   `(validate-ref ~'subj ~rf))
 
+(defmethod expand-key :extends
+  [cctx _ rf _])
+
+(defmethod expand-key :format
+  [cctx _ rf _])
+
+(defmethod expand-key :dependencies
+  [cctx _ rf _])
+
+(defmethod expand-key :divisibleBy
+  [cctx _ rf _])
+
+(defn reduce-indexed [acc f col]
+  (loop [[x & xs] col i 0 acc acc]
+    (if (empty? xs)
+      (f acc x i)
+      (recur xs (inc i) (f acc x i)))))
+
+
+(defmethod expand-key :items
+  [cctx _ its _]
+  (cond (map? its)
+        (let [it-sch (expand cctx its)]
+          `(cond->
+            (sequential? ~'subj)
+            (reduce-indexed
+             (fn [ctx# x# i#]
+               (-> ctx#
+                   (push-path i#)
+                   (~it-sch x#)
+                   (pop-path)))
+             ~'subj)))
+        :else (println "TODO items" its)))
+
 (defn expand [cctx sch]
-  (let [pipe (mapv (fn [[k ksch]] (expand-key cctx k ksch sch)) sch)
+  (let [pipe (->> sch
+                  (mapv (fn [[k ksch]] (expand-key (push-path cctx k) k ksch sch)))
+                  (filterv #(not (nil? %))))
         lmbd `(fn [~'ctx ~'subj] (-> ~'ctx ~@pipe))]
     (when-let [$ref (:$ref cctx)]
       (swap! $ref assoc (str "#/" (str/join "/" (mapv name (:path cctx)))) lmbd))
     lmbd))
 
-(defn add-error [ctx msg]
-  (update ctx :errors conj {:desc msg :path (:path ctx)}))
 
+;; to track $ref 
+;; we create refs atom which is filled by compilation
+;; and used in validation
 (defn compile-schema [sch]
   (let [refs (atom {})
         cctx {:path [] :$ref refs}
         f (eval (expand cctx sch))]
     (fn [subj]
       (->
-       (f {:path [] :$ref refs} subj)
-       (dissoc :$ref)))))
+       (f {:path [] :errors [] :$ref refs} subj)
+       (dissoc :$ref :path)))))
 
 (defn test [sch subj]
   (let [sch-f (compile-schema sch)]
     (sch-f subj)))
 
 
-
+(expand {} {:items {:type "string"}})
 
 
 
--- test/json_schema/macroschema_test.clj
+++ test/json_schema/macroschema_test.clj
@@ -1,5 +1,6 @@
 (ns json-schema.macroschema-test
   (:require [json-schema.macroschema :as sut]
+            [json-schema.utils :as u]
             [clojure.test :refer :all]))
 
 (deftest test-macroschema
@@ -7,22 +8,21 @@
 
   (def rsch {:required ["a" "b" "c"]})
 
-  (is (= [{:desc "c is required" :path []}
+  (is (= [{:desc "a is required" :path []}
           {:desc "b is required" :path []}
-          {:desc "a is required" :path []}]
-         (:errors (sut/test rsch {}))
-         ))
+          {:desc "c is required" :path []}]
+         (:errors (sut/test rsch {}))))
 
-  (is (= [{:desc "b is required" :path []}
-          {:desc "a is required" :path []}]
+  (is (= [{:desc "a is required" :path []}
+          {:desc "b is required" :path []}]
          (:errors (sut/test rsch {:c false}))))
 
-  (is (nil? (:errors (sut/test rsch 1 ))))
+  (is (empty? (:errors (sut/test rsch 1 ))))
 
   (def tsch {:type "object"})
 
   (is (= [{:desc "Expected an object, got 1" :path []}]
-        (:errors (sut/test tsch 1))))
+         (:errors (sut/test tsch 1))))
 
   (is (= [{:desc "Expected an array, got 1" :path []}]
          (:errors (sut/test {:type "array"} 1))))
@@ -30,6 +30,8 @@
   (is (= [{:desc "Expected a string, got 1" :path []}]
          (:errors (sut/test {:type "string"} 1))))
 
+  (def tsch {:type ["object" "string"]})
+  (sut/test tsch 1)
 
 
   (sut/test {:properties {:a {:type "string"}}} {:a 1})
@@ -44,20 +46,112 @@
                   :c {:type "object"
                       :properties {:e {:type "string"}}}}})
 
-  (is (= [{:desc "Expected a string, got true", :path [:c :e]}
+  (is (= [{:desc "b is required", :path []}
           {:desc "Expected a string, got 1", :path [:a]}
-          {:desc "b is required", :path []}]
+          {:desc "Expected a string, got true", :path [:c :e]}]
          (:errors (sut/test prop-sch {:a 1 :c {:e true}}))))
 
 
-  (is (= [{:desc "Expected a string, got 1", :path [:b]}
-          {:desc "Expected a string, got 1", :path [:a]}]
+  (is (= [{:desc "Expected a string, got 1", :path [:a]}
+          {:desc "Expected a string, got 1", :path [:b]}]
          (:errors
           (sut/test
            {:properties {:a {:type "string"}
                          :b {:$ref "#/properties/a"}}}
-           {:a 1 :b 1}))))
+           {:a 1 :b 1}))
+         ))
+
+  (is (= [{:desc "Expected a string, got 1", :path [0]}
+          {:desc "Expected a string, got 2", :path [1]}
+          {:desc "Expected a string, got 3", :path [2]}]
+         (:errors (sut/test {:items {:type "string"}} [1 2 3]))))
+
+  (is (empty? (:errors (sut/test {:items {:type "string"}} "a"))))
+  
+  )
 
 
+
+(def re-filter #"^.*$")
+
+(deftest focus-3-test
+
+  ;; (defn compile [sch] (sut/compile-schema sch))
+
+  ;; (let [validator (compile {:properties {:bar {:type "integer", :required true}},
+  ;;                           :extends {:properties {:foo {:type "string", :required true}}}})
+  ;;       res (validator {:bar 2})]
+  ;;   (is (= false (empty? (:errors res))))
+  ;;   res)
+
+  ;; (let [validator (compile {:properties {:bar {:type "integer", :required true}},
+  ;;                           :extends
+  ;;                           [{:properties {:foo {:type "string", :required true}}}
+  ;;                            {:properties {:baz {:type "string", :required true}}}]})
+  ;;       res (validator {:foo "quux", :bar 2, :baz "foo"})]
+  ;;   (is (= true (empty? (:errors res))))
+  ;;   res)
+
+  ;; ;; validation of CSS colors
+  ;; (let [validator (compile {:format "color"})
+  ;;       res (validator "#CC8899")]
+  ;;   (is (= true (empty? (:errors res))))
+  ;;   res)
+
+  ;; ;; validation of CSS colors
+  ;; (let [validator (compile {:format "color"})
+  ;;       res (validator "#00332520")]
+  ;;   (is (= false (empty? (:errors res))))
+  ;;   res)
+
+  ;; ;;dependencies
+  ;; (let [validator (compile {:dependencies {:bar "foo"}})
+  ;;       res (validator {:bar 2})]
+  ;;   (is (= false (empty? (:errors res))))
+  ;;   res)
+
+
+  ;; ;; ECMA 262 regex dialect recognition
+  ;; (let [validator (compile {:format "regex"})
+  ;;       res (validator "(?<=foobar")]
+  ;;   (is (= false (empty? (:errors res))))
+  ;;   res)
+
+  ;; "by small number"
+  ;; (let [validator (compile {:divisibleBy 2})
+  ;;       res (validator 4)]
+  ;;   (is (= true (empty? (:errors res))))
+  ;;   res)
+
+  ;; (let [validator (compile {:divisibleBy 3})
+  ;;       res (validator 4)]
+  ;;   (is (= false (empty? (:errors res))))
+  ;;   res)
+
+  ;; "types can include schemas"
+  ;; (let [validator (compile {:type ["array" {:type "object"}]})
+  ;;       res (validator {})]
+  ;;   (is (= true (empty? (:errors res))))
+  ;;   res)
+
+  ;; "remote ref"
+  ;; (u/with-server
+  ;;   (fn []
+  ;;     (let [validator (compile {:$ref "http://localhost:1234/integer.json"})
+  ;;           res (validator "a")]
+  ;;       (is (= false (empty? (:errors res))))
+  ;;       res)))
+
+  ;; (u/with-server
+  ;;   (let [validator (compile {:id "http://localhost:1234/",
+  ;;                             :items {:id "folder/", :items {:$ref "folderInteger.json"}}})
+  ;;         res (validator [[1]])]
+  ;;     (is (= true (empty? (:errors res))))
+  ;;     res))
+
+  )
+
+(deftest draft3-test
+  ;; (u/test-files (u/files "draft3" re-filter) #{"multiple extends" "ECMA 262 regex dialect recognition"})
   )
 
